d067534b82a290e3ad6c77c1d898276c1aa853ca
a-reznikov <a-reznnikov@gmail.com>
2023-04-14 12:31:16 +0300

    feat: implement solution for remove-from-list

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -22,10 +22,98 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.length = 0;
+  }
 }
+
+function removeKFromList(node, k) {  
+  // console.log('-------CURRENT', node, 'CURRENT-------');
+  let current = node;
+  let length = coutnLength(node);
+  // console.log(length);
+  // console.log(current);
+  let prev = null;
+  let index = 0;
+
+  while (current) {
+    // console.log(node.value);
+    if (current.value === k && index === 0) {
+      node = current.next;
+      current = node;
+      index ++;
+    }
+    else if (current.value === k && current.next === null) {
+      // console.log('Find but null');
+      prev.next = null;
+      index ++;
+    } else if (current.value === k) {
+      // console.log('FIND = ',current);
+      prev.next = current.next;
+      index ++;
+    } else if (prev != null && prev.value === k){
+      index --;
+    } else {
+      index ++;
+    }
+    prev = current;
+    current = current.next;
+  }
+  // console.log('-------K', k, 'K-------');
+  // console.log('index', index);
+  // console.log('length', length);
+  // console.log('!!!!!!!!!!!!\n', node, '\n!!!!!!!!!!!!');
+  if (index !== length) {
+    removeKFromList(node, k);
+  }
+  // print(node);
+  return node;
+}
+
+function print(node) {
+  let current = node;
+  while (current) {
+    console.log(current.value);
+    current = current.next;
+  }
+}
+
+function coutnLength(node) {
+  let current = node;
+  let len = 0;
+  while (current) {
+    len ++;
+    current = current.next;
+  }
+  return len;
+}
+
+
+
+
+// let list = {
+//   value: 1,
+//   next: {
+//     value: 2,
+//     next: {
+//       value: 3,
+//       next: {
+//         value: 3,
+//         next: {
+//           value: 5,
+//           next: {
+//             value: 3,
+//             next: null
+//           }
+//         }
+//       }
+//     }
+//   }
+// };
+
+// removeKFromList(list, 3);
 
 module.exports = {
   removeKFromList
